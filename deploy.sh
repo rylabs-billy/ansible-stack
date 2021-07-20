5980847a6fc87230caaa457238c1d231e30b5722
Billy Thompson <bthompson@linode.com>
2021-07-20 10:17:44 -0400

    update deploy script again

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
-exec > /dev/ttyS0 2>&1
+set -e
+trap "cleanup $? $LINENO" EXIT
 
-virtualenv --python=python3 env
-source env/bin/activate
-pip install -r requirements.txt
-ansible-galaxy collection install linode.cloud community.crypto community.mysql
+exec > >(tee /dev/ttyS0 /var/log/ansible.log) 2>&1
 
-# make secrets
-TEMP_ROOT_PASS=$(openssl rand -base64 32)
-ansible-vault encrypt_string ${TEMP_ROOT_PASS} --name 'root_pass' > group_vars/galera/secret_vars
-ansible-vault encrypt_string $1 --name 'token' >> group_vars/galera/secret_vars
+function cleanup {
+  if [ "$?" != "0" ]; then
+    echo "PLAYBOOK FAILED. See /var/log/ansible.log for details."
+    deactivate
+    rm -rf env
+    exit 1
+  fi
+}
 
-# run  playbook
-ansible-playbook provision.yml -vvv
+function run_playbook {
+  # set up virtual environment
+  virtualenv --python=python3 env
+  source env/bin/activate
+  
+  # install requirements
+  pip install -r requirements.txt
+  ansible-galaxy collection install linode.cloud community.crypto community.mysql
+
+  # write secret vars
+  TEMP_ROOT_PASS=$(openssl rand -base64 32)
+  ansible-vault encrypt_string ${TEMP_ROOT_PASS} --name 'root_pass' > group_vars/galera/secret_vars
+  ansible-vault encrypt_string $1 --name 'token' >> group_vars/galera/secret_vars
+
+  # run provision playbook
+  ansible-playbook provision.yml
+
+  # run galera playbook
+  ansible-playbook -i hosts site.yml --extra-vars "root_password=$2 account_ssh_keys=$3 add_keys_prompt=$4"
+
+  # deactivate virtual environment
+  deactivate
+  rm -rf env
+}
+
+# main
+run_playbook
