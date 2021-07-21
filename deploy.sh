e761be3e78c0a2f3fd291554142ab1fae4b6390d
Billy Thompson <bthompson@linode.com>
2021-07-21 15:26:14 -0400

    localhost it

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ function cleanup {
 function run_playbook {
   # run provision playbook
   #echo "private_key_file = $HOME/.ssh/id_ansible_ed2551" >> ansible.cfg
-  ansible-playbook provision.yml
+  ansible-playbook provision.yml --extra-vars "localhost_public_ip=${PUBLIC_IP} localhost_private_ip=${PRIVATE_IP}"
   # run galera playbook
   ansible-playbook -i hosts site.yml --extra-vars "root_password=${ROOT_PASS} account_ssh_keys=${SSH_KEYS} add_keys_prompt=${ADD_SSH_KEYS}"
 }
