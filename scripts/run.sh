c3a8720995bf368e4d1e2848a5114b36bb0bf2f6
Billy Thompson <bthompson@linode.com>
2021-10-19 14:38:06 -0400

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -89,7 +89,7 @@ function ansible:deploy {
   #echo -e "\nprivate_key_file = ${ANSIBLE_SSH_KEY_PATH}" >> ansible.cfg
   ansible-playbook provision.yml --extra-vars "localhost_public_ip=${PUBLIC_IP} localhost_private_ip=${PRIVATE_IP}" --flush-cache
   # run galera playbook
-  ansible-playbook -i hosts site.yml --extra-vars "root_password=${ROOT_PASS} add_keys_prompt=${ADD_SSH_KEYS}"
+  ansible-playbook -i hosts site.yml -vvv --extra-vars "root_password=${ROOT_PASS} add_keys_prompt=${ADD_SSH_KEYS}"
 }
 
 function build_old {
